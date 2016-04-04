729b7e228049828a55e6db080fbe07f3c99eecfe
johannschill <joh.schill@gmail.com>
2016-04-04 20:51:28 +0200

    Add Featured Post

--- index.php
+++ index.php
@@ -1,7 +1,52 @@
 <?php get_header(); ?>
 
+
+
+<?php query_posts('cat=2'); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+
+  <div class="item featured">
+    <div class="row">
+      <div class="col-xs-12 reset-padding">
+
+        <div class="headline"><h1><?php the_title(); ?></h1></div>
+
+        <div class="item-content">
+          <div class="item-content-img">
+            <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+
+              <?php if ( has_post_thumbnail() ) {
+              	the_post_thumbnail();
+              } ?>
+
+            </a>
+
+          </div>
+        </div>
+
+        <div class="item-cta ">
+          <a class="gumroad-button" href="<?php the_field('gumroad_url'); ?>"><?php the_field('gumroad_button_text'); ?></a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+<?php endwhile; endif; ?>
+<?php wp_reset_query(); ?>
+
+
+
+
+
+
+
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 
 <div class="item">
@@ -30,10 +75,6 @@
     </div>
   </div>
 </div>
-
-
-
-
 
 
 
