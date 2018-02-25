a54528ff77e221d8d2a0ac633f7b1965f7315d9e
Johann Schill <joh.schill@gmail.com>
2018-02-25 14:42:19 +0100

    Test

--- footer.php
+++ footer.php
@@ -11,6 +11,7 @@
             <ul>
               <li><a href="<?php echo get_page_link(7); ?>"><?php echo get_the_title(7); ?></a></li>
             </ul>
+            
 
             <div class="copyright">
               © justmechs.com <?php echo date('Y'); ?>
